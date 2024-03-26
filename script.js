6461596789af0575b3cc1725a6c50cf0dae17b5f
Ben <157050521+bbeattie-geospatial@users.noreply.github.com>
2024-03-26 12:57:38 +0000

    Mar 26, 2024, 12:57 PM

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function Subtract(e) {
-  let inputValue = document.getElementById("inputbox");
-  console.log(inputValue.value);
-  let newValue = inputValue.value - 1;
-  console.log(newValue);
-}
+const decreasebtn = document.getElementById("decrease");
+const increasebtn = document.getElementById("increase");
+const resetbtn = document.getElementById("reset");
+const showlogsbtn = document.getElementById("showlogs");
+const inputbox = document.getElementById("inputbox");
+
+decreasebtn.onclick(console.log("Hello"));
