a1e698bcab3737ff3acdfe61f4b02d3eb7d621cc
Ben <157050521+bbeattie-geospatial@users.noreply.github.com>
2024-03-26 13:04:27 +0000

    Mar 26, 2024, 1:04 PM

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const increasebtn = document.getElementById("increase");
 const resetbtn = document.getElementById("reset");
 const showlogsbtn = document.getElementById("showlogs");
 const inputbox = document.getElementById("inputbox");
+let initialvalue = 0;
 
-decreasebtn.addEventListener("click", console.log("Hello"));
+decreasebtn.addEventListener("click", () => {
+  console.log("hello");
+  console.log(inputbox.value);
+});
